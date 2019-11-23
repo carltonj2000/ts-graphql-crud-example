aaa22bda21bbdf0e0312a474cf5664bd869ce01f
Carlton Joseph <carlton.joseph@gmail.com>
2019-11-22 18:10:44 -0800

    add movie delete functionality

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -19,6 +19,15 @@ class MovieInput {
   minutes: number;
 }
 
+@InputType()
+class MovieUpdateInput {
+  @Field(() => String, { nullable: true })
+  title?: string;
+
+  @Field(() => Int, { nullable: true })
+  minutes?: number;
+}
+
 @Resolver()
 export class MovieResolver {
   @Mutation(() => Movie)
@@ -30,9 +39,15 @@ export class MovieResolver {
   @Mutation(() => Boolean)
   async updateMovie(
     @Arg("id", () => Int) id: number,
-    @Arg("input", () => MovieInput) input: MovieInput
+    @Arg("input", () => MovieUpdateInput) input: MovieUpdateInput
   ) {
     await Movie.update({ id }, input);
+    return true;
+  }
+
+  @Mutation(() => Boolean)
+  async deleteMovie(@Arg("id", () => Int) id: number) {
+    await Movie.delete(id);
     return true;
   }
 
